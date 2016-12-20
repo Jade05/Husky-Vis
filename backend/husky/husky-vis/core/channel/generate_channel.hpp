5008747e09d874da1092fa05c0dcc63c4135b29a
Yuying GUO <bdgoocci@gmail.com>
2016-12-20 17:57:00 +0800

    channel and constants

--- backend/husky/husky-vis/core/channel/generate_channel.hpp
+++ backend/husky/husky-vis/core/channel/generate_channel.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <boost/property_tree/ptree.hpp>
+
 namespace husky {
 namespace vis {
 namespace core {
 namespace channel {
+	using namespace boost::property_tree;
+	
 	class GenerateChannel {
 		
 	}
