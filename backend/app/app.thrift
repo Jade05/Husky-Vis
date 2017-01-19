8768ec69c6671d2edc63504b64ecb3b099057ea9
guoyuying <bdgoocci@gmail.com>
2017-01-19 12:23:59 +0800

    modify app.thrift

--- backend/app/app.thrift
+++ backend/app/app.thrift
@@ -1,6 +1,29 @@
 namespace cpp Visualization  
-  
+
+struct Suggestion {
+    1:required string measure,
+    2:required string dimension,
+    3:required string chart_type,
+    4:required string aggregate_type,
+    5:require string statistical_method,
+    6:required map<string, list<string>> group_by_raw_data,
+    7:required map<string, double> aggregate_data,
+    8:required double score        
+}
+
+struct SuggestionsSet{
+    1:required list<Suggestion> allSuggestions
+}
+
+struct Attribute {
+    1:required string attribute
+}
+
+struct AttributesSet {
+    1:required list<Attribute> allAttributes
+}
+
 service App {  
-    i32 ping(); 
+    SuggestionSet ping(); 
     list<string> init_run();
 } 
