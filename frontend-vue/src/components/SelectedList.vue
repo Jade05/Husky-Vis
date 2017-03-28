3965b7d0d4197beb5c403ec398a79ed0333a3f53
Yuying Guo <Yuying Guo>
2017-03-28 09:01:59 +0800

    tmp

---
+++ frontend-vue/src/components/SelectedList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-for="attribute in attributes" class="checkbox">
+      <label>
+          <input class="pane-left-attribute" type="checkbox" name="{{attribute}}" value="{{attribute}}">
+          <span class="attribute-name">{{attribute}}</span>
+      </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectedList',
+  data： {
+     attributes: [
+         "Name",
+         "Year",
+         "Month",
+         "Day"
+     ]
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import SelectedList from '@/components/SelectedList'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'Hello',
             component: Hello
+        },
+        {
+            path: '/test',
+            name: '',
+            component: SelectedList
         }
     ]
 })
