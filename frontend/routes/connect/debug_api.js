861971f8d3118801cc25b6d3f1802a101ead47ce
Yuying GUO <bdgoocci@gmail.com>
2017-03-29 10:40:09 +0800

    [frontend][routes][connect] add debug files to connect thrift c++ api to debug without needing by browser

--- frontend/routes/connect/debug_api.js
+++ frontend/routes/connect/debug_api.js
@@ -2,7 +2,12 @@ var connect = require("./connect");
 
 connect.init();
 
+var response;
 connect.get_suggestions(function(response) {
     console.log(response);
 });
 
+
+//connect.get_attributes(function(response) {
+//    console.log(response);
+//});
