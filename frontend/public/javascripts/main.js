7aa37dc8160bd2b84f6cb26bb3d83bd91df6ad41
GUOYuying <bdgoocci@gmail.com>
2017-04-07 00:23:24 +0800

    [frontend] delete old frontend

--- frontend/public/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-requirejs.config({
-    //By default load any module IDs from js/lib
-    baseUrl: 'javascripts/lib',
-    paths: {
-        app: '../javascripts'
-    }
-});
--- frontend/routes/init.js
+++
@@ -1,7 +0,0 @@
-var index = require("./index");
-var selectAttribute = require("./selectAttribute");
-
-exports.init = function(app) {
-    app.use('/', index);
-    app.post('/selectAttribute', selectAttribute.selectAttribute);
-};
